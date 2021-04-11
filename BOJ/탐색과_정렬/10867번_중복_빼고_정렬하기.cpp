acc9e655938619e6e1e79c484eec3d781e7b302d
Komputer-P <wlghks0314@gmail.com>
2021-04-11 20:29:45 +0900

    BOJ

---
+++ BOJ/탐색과_정렬/10867번_중복_빼고_정렬하기.cpp
@@ -0,0 +1,50 @@
+/*
+BOJ 10867번: 중복 빼고 정렬하기
+
+시간 제한: 1초
+메모리 제한: 256 MB
+
+문제
+N개의 정수가 주어진다. 이때, N개의 정수를 오름차순으로 정렬하는 프로그램을 작성하시오. 같은 정수는 한 번만 출력한다.
+
+입력
+첫째 줄에 수의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째에는 숫자가 주어진다. 이 수는 절댓값이 1,000보다 작거나 같은 정수이다.
+
+출력
+첫째 줄에 수를 오름차순으로 정렬한 결과를 출력한다. 이때, 같은 수는 한 번만 출력한다.
+*/
+
+//#define _CRT_SECURE_NO_WARNINGS
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> v;
+int n;
+int num;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+	v.erase(unique(v.begin(), v.end()), v.end());
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << " ";
+	}
+}
+
+/*
+중복 제거, 정렬
+*/
